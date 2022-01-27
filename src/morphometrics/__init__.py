00252d91da2a5fc48adae4a5dc97cb0420f15105
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-01-27 14:53:03 +0100

    add sample data plugin

--- src/morphometrics/__init__.py
+++ src/morphometrics/__init__.py
@@ -1,11 +1,8 @@
-
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
 
 
-
-
-from ._sample_data import make_sample_data 
+from ._sample_data import make_simple_labeled_cube
 from ._widget import ExampleQWidget, example_magic_widget
--- src/morphometrics/_sample_data.py
+++ src/morphometrics/_sample_data.py
@@ -1,15 +1,8 @@
-"""
-This module is an example of a barebones sample data provider for napari.
+from typing import List
 
-It implements the "sample data" specification.
-see: https://napari.org/plugins/stable/npe2_manifest_specification.html
-
-Replace code below according to your needs.
-"""
-from __future__ import annotations
-import numpy
+from .data import simple_labeled_cube
 
 
-def make_sample_data():
-    """Generates an image"""
-    return numpy.random.rand(512, 512)
+def make_simple_labeled_cube() -> "List[napari.types.LayerDataTuple]":  # noqa: F821
+    """Create a simple labeled cube"""
+    return [(simple_labeled_cube(), {}, "labels")]
