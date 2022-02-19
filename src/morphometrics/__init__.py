3d7e3aa3b0fadef7556bfa8880bcc44cd539172d
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-02-19 12:33:46 +0100

    update sample data

--- src/morphometrics/__init__.py
+++ src/morphometrics/__init__.py
@@ -4,5 +4,4 @@ except ImportError:
     __version__ = "unknown"
 
 
-from ._sample_data import make_simple_labeled_cube
-from ._widget import ExampleQWidget, example_magic_widget
+from ._sample_data import make_simple_labeled_cube, random_3d_image
--- src/morphometrics/_sample_data.py
+++ src/morphometrics/_sample_data.py
@@ -1,8 +1,12 @@
 from typing import List
 
-from .data import simple_labeled_cube
+from .data import random_3d_image, simple_labeled_cube
 
 
 def make_simple_labeled_cube() -> "List[napari.types.LayerDataTuple]":  # noqa: F821
     """Create a simple labeled cube"""
     return [(simple_labeled_cube(), {}, "labels")]
+
+
+def make_random_3d_image() -> "List[napari.types.LayerDataTuple]":  # noqa: F821
+    return [(random_3d_image(), {}, "image")]
