125f2fd85c777c4e292034394a80cb95ec40f4ba
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-03-15 16:03:37 +0100

    add surface distance calculation

--- src/morphometrics/measure/mesh.py
+++ src/morphometrics/measure/mesh.py
@@ -1,6 +1,9 @@
+from typing import Union
+
 import numpy as np
 import pandas as pd
 import pyvista as pv
+import trimesh
 
 
 def measure_surface_properties(mesh: pv.PolyData) -> pd.DataFrame:
@@ -28,3 +31,47 @@ def measure_surface_properties(mesh: pv.PolyData) -> pd.DataFrame:
             }
         ]
     )
+
+
+def distance_between_surfaces(
+    source_surface: trimesh.Trimesh,
+    destination_surface: trimesh.Trimesh,
+    fill_value: Union[float] = 0,
+    flip_normals: bool = False,
+) -> np.ndarray:
+    """Calculate the per-vertex distance between two surfaces.
+
+    Parameters
+    ----------
+    source_surface : trimesh.Trimesh
+        The surface to calculate the distance from.
+    destination_surface : trimesh.Trimesh
+        The surface to calcuate the distance to.
+    fill_value : Union[float]
+        Value to fill in for the distances where the ray doesn't
+        intersect the distination_surface.
+    flip_normals : bool
+        If True, normal vectors will be flipped.
+        Default value is False.
+
+    Returns
+    -------
+    distances : np.ndarray
+        Distance from each vertex in source_surface to the
+        destination_surface along the vertex normals of source_surface.
+    """
+    ray_origins = source_surface.vertices
+    ray_directions = source_surface.vertex_normals
+    if flip_normals is True:
+        ray_directions = -1 * ray_directions
+
+    # find where the rays intersect
+    locations, index_ray, _ = destination_surface.ray.intersects_location(
+        ray_origins=ray_origins, ray_directions=ray_directions
+    )
+
+    # calculate the distances
+    distances = np.repeat(fill_value, len(ray_origins))
+    distances[index_ray] = np.linalg.norm(locations - ray_origins[index_ray])
+
+    return distances
