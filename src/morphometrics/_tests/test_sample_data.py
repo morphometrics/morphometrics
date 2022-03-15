bc291544a4e30469e503a6951222d888aaae870b
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-03-15 08:21:44 +0100

    update sample data tests

--- src/morphometrics/_tests/test_sample_data.py
+++ src/morphometrics/_tests/test_sample_data.py
@@ -1,7 +1,22 @@
-# from morphometrics import make_sample_data
+from morphometrics._sample_data import make_random_3d_image, make_simple_labeled_cube
 
 # add your tests here...
 
 
-def test_something():
-    pass
+def test_random_3d_image():
+    layer_data_list = make_random_3d_image()
+    assert len(layer_data_list) == 1
+
+    data, meta, layer_type = layer_data_list[0]
+    assert data.ndim == 3
+    assert layer_type == "image"
+
+
+def test_simple_labeled_cube():
+    layer_data_list = make_simple_labeled_cube()
+    assert len(layer_data_list) == 1
+
+    data, meta, layer_type = layer_data_list[0]
+    assert data.ndim == 3
+    assert data.dtype == int
+    assert layer_type == "labels"
