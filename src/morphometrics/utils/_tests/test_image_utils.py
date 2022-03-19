593762f87ce359121e39e71803b4f563caee319e
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-03-19 11:08:22 +0100

    add tests

--- src/morphometrics/utils/_tests/test_image_utils.py
+++ src/morphometrics/utils/_tests/test_image_utils.py
@@ -3,7 +3,7 @@ import pytest
 
 from morphometrics.utils.image_utils import make_boundary_mask
 
-label_image = np.array(
+label_image_2 = np.array(
     [
         [0, 0, 0, 0, 0, 0],
         [0, 1, 1, 1, 1, 0],
@@ -13,6 +13,9 @@ label_image = np.array(
         [0, 0, 0, 0, 0, 0],
     ]
 )
+
+label_image_3d = np.zeros((6, 6, 6), dtype=int)
+label_image_3d[1:5, 1:5, 1:5] = 1
 
 
 def test_make_boundary_mask_no_dilation():
@@ -26,11 +29,11 @@ def test_make_boundary_mask_no_dilation():
             [0, 0, 0, 0, 0, 0],
         ]
     )
-    boundary_mask = make_boundary_mask(label_image, boundary_dilation_size=0)
+    boundary_mask = make_boundary_mask(label_image_2, boundary_dilation_size=0)
     np.testing.assert_array_equal(boundary_mask, expected_label_image)
 
 
-def test_make_boundary_mask_dilation():
+def test_make_boundary_mask_dilation_2d():
     expected_label_image = np.array(
         [
             [0, 0, 0, 0, 0, 0],
@@ -41,7 +44,15 @@ def test_make_boundary_mask_dilation():
             [0, 0, 0, 0, 0, 0],
         ]
     )
-    boundary_mask = make_boundary_mask(label_image, boundary_dilation_size=3)
+    boundary_mask = make_boundary_mask(label_image_2, boundary_dilation_size=3)
+    np.testing.assert_array_equal(boundary_mask, expected_label_image)
+
+
+def test_make_boundary_mask_dilation_3d():
+    expected_label_image = np.zeros((6, 6, 6), dtype=int)
+    expected_label_image[1:5, 1:5, 1:5] = 1
+
+    boundary_mask = make_boundary_mask(label_image_3d, boundary_dilation_size=3)
     np.testing.assert_array_equal(boundary_mask, expected_label_image)
 
 
