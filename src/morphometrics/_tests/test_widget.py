4a3d22df600e97f48300ed7ae10f1621082618c5
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-04-20 17:39:00 +0200

    add dock widget plugin

--- src/morphometrics/_tests/test_widget.py
+++ src/morphometrics/_tests/test_widget.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+import morphometrics
 from morphometrics._gui._qt.annotation_widgets import QtClusterAnnotatorWidget
 from morphometrics._gui._qt.measurement_widgets import QtMeasurementWidget
 
@@ -14,3 +15,22 @@ def test_creating_widget(make_napari_viewer, widget):
 
     # create our widget, passing in the viewer
     _ = widget(viewer)
+
+
+MY_PLUGIN_NAME = "morphometrics"
+# the name of your widget(s)
+MY_WIDGET_NAMES = [
+    "Annotate clustered features",
+    "Measure region properties",
+]
+
+
+@pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
+def test_something_with_viewer(widget_name, make_napari_viewer, napari_plugin_manager):
+    napari_plugin_manager.register(morphometrics, name=MY_PLUGIN_NAME)
+    viewer = make_napari_viewer()
+    num_dw = len(viewer.window._dock_widgets)
+    viewer.window.add_plugin_dock_widget(
+        plugin_name=MY_PLUGIN_NAME, widget_name=widget_name
+    )
+    assert len(viewer.window._dock_widgets) == num_dw + 1
