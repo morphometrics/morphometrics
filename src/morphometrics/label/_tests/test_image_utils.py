33373144849a248da61c393ca24c514c044aee13
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-05-28 11:34:42 +0200

    add function to expande selected labels

---
+++ src/morphometrics/label/_tests/test_image_utils.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pytest
+
+from morphometrics.label.image_utils import expand_selected_labels
+
+
+def test_expand_selected_labels_2d():
+    label_image = np.zeros((100, 100), dtype=int)
+    label_image[30:70, 30:70] = 1
+    label_image[45:55, 45:55] = 0
+
+    label_image[49:51, 49:51] = 2
+
+    expanded_image = expand_selected_labels(
+        label_image=label_image, label_values_to_expand=2, expansion_amount=1
+    )
+
+    # check that the unexpanded labels were not expanded
+    np.testing.assert_equal(label_image.shape, expanded_image.shape)
+    np.testing.assert_equal(label_image == 1, expanded_image == 1)
+
+    assert expanded_image[50, 52] == 2
+
+    # expand to fill the void
+    expanded_image_2 = expand_selected_labels(
+        label_image=label_image, label_values_to_expand=2, expansion_amount=5
+    )
+    np.testing.assert_equal(label_image.shape, expanded_image_2.shape)
+    np.testing.assert_equal(label_image == 1, expanded_image_2 == 1)
+
+    # check that the entire void was filled with 2
+    assert np.sum(expanded_image_2 == 2) == 10 ** 2
+
+
+def test_expand_selected_labels_3d():
+    label_image = np.zeros((100, 100, 100), dtype=int)
+    label_image[30:70, 30:70, 30:70] = 1
+    label_image[45:55, 45:55, 45:55] = 0
+
+    label_image[49:51, 49:51, 49:51] = 2
+
+    expanded_image = expand_selected_labels(
+        label_image=label_image, label_values_to_expand=2, expansion_amount=1
+    )
+
+    np.testing.assert_equal(label_image.shape, expanded_image.shape)
+    np.testing.assert_equal(label_image == 1, expanded_image == 1)
+
+    assert expanded_image[50, 52, 50] == 2
+
+    # expand to fill the void
+    expanded_image_2 = expand_selected_labels(
+        label_image=label_image, label_values_to_expand=2, expansion_amount=5
+    )
+    np.testing.assert_equal(label_image.shape, expanded_image_2.shape)
+    np.testing.assert_equal(label_image == 1, expanded_image_2 == 1)
+
+    # check that the entire void was filled with 2
+    assert np.sum(expanded_image_2 == 2) == 10 ** 3
+
+
+def test_expand_selected_labels_4d():
+    """Should raise a value error with label ndim > 3"""
+    label_image = np.zeros((10, 10, 10, 10), dtype=int)
+
+    with pytest.raises(ValueError):
+        _ = expand_selected_labels(
+            label_image=label_image, label_values_to_expand=2, expansion_amount=1
+        )
+
+
+def test_expand_selected_labels_background_mask_2d():
+    label_image = np.zeros((100, 100), dtype=int)
+    label_image[49:51, 49:51] = 2
+
+    background_mask = np.ones_like(label_image, dtype=bool)
+    background_mask[45:55, 45:55] = 0
+
+    expanded_image = expand_selected_labels(
+        label_image=label_image,
+        label_values_to_expand=2,
+        expansion_amount=10,
+        background_mask=background_mask,
+    )
+
+    # check that the shape was preserved
+    np.testing.assert_equal(label_image.shape, expanded_image.shape)
+
+    # check that label 2 was only expanded to the edge of the boundary mask
+    assert np.sum(expanded_image == 2) == 10 ** 2
+
+
+def test_expand_selected_labels_background_mask_3d():
+    label_image = np.zeros((100, 100, 100), dtype=int)
+    label_image[49:51, 49:51, 49:51] = 2
+
+    background_mask = np.ones_like(label_image, dtype=bool)
+    background_mask[45:55, 45:55, 45:55] = 0
+
+    expanded_image = expand_selected_labels(
+        label_image=label_image,
+        label_values_to_expand=2,
+        expansion_amount=10,
+        background_mask=background_mask,
+    )
+
+    # check that the shape was preserved
+    np.testing.assert_equal(label_image.shape, expanded_image.shape)
+
+    # check that label 2 was only expanded to the edge of the boundary mask
+    assert np.sum(expanded_image == 2) == 10 ** 3
---
+++ src/morphometrics/label/image_utils.py
@@ -0,0 +1,91 @@
+from typing import List, Optional, Union
+
+import numpy as np
+import pyclesperanto_prototype as cle
+
+from ..types import LabelImage
+
+
+def expand_selected_labels(
+    label_image: LabelImage,
+    label_values_to_expand: Union[int, List[int]],
+    expansion_amount: int = 1,
+    background_mask: Optional[np.ndarray] = None,
+) -> LabelImage:
+    """Expand selected label values in a label image.
+
+    Parameters
+    ----------
+    label_image : LabelImage
+        The label image to expand selected values in. Must be 2D or 3D.
+    label_values_to_expand : Union[int, List[int]]
+        The values in the label image that are allowed to expand.
+    expansion_amount : int
+        The radius of the expansion. Expansion is performed with equal
+        amounts in all directions.
+    background_mask : Optional[np.ndarray]
+        A boolean mask where True denotes background into which the labels
+        cannot be expanded. If None, the background mask is not applied.
+        Default value is None
+
+    Returns
+    -------
+    expanded_label_image : LabelImage
+        The resulting label image with the selected labels expanded.
+    """
+
+    if isinstance(label_values_to_expand, int):
+        # coerce into list
+        label_values_to_expand = [label_values_to_expand]
+
+    if label_image.ndim == 2:
+        label_image_is_2d = True
+        label_image = np.expand_dims(label_image, axis=0)
+    elif label_image.ndim == 3:
+        label_image_is_2d = False
+    else:
+        raise ValueError("label_image must be 2D or 3D")
+
+    gpu_labels = cle.push(label_image)
+
+    # preallocate the memory
+    flip = cle.create_labels_like(gpu_labels)
+    flop = cle.create_labels_like(gpu_labels)
+    flap = cle.create_labels_like(gpu_labels)
+    labels_mask = cle.create_labels_like(gpu_labels)
+    expanded_labels = cle.create_labels_like(gpu_labels)
+
+    kernel_size = cle.create([1, 1, 1])
+
+    expand_mask = cle.create_binary_like(gpu_labels)
+    cle.equal_constant(gpu_labels, expand_mask, 0)
+    # cle.binary_or(expand_mask, labels_mask, expand_mask)
+
+    for label_value in label_values_to_expand:
+        cle.equal_constant(gpu_labels, labels_mask, label_value)
+        cle.binary_or(expand_mask, labels_mask, expand_mask)
+
+    # create the labels to expand
+    cle.multiply_images(gpu_labels, expand_mask, flip)
+
+    for i in range(expansion_amount):
+        # dilate the labels
+        cle.onlyzero_overwrite_maximum_box(flip, kernel_size, flop)
+        cle.onlyzero_overwrite_maximum_diamond(flop, kernel_size, flap)
+
+        # remove the expansion that occurred where other labels already are
+        cle.multiply_images(flap, expand_mask, flip)
+
+    cle.binary_not(expand_mask, labels_mask)
+    cle.multiply_images(gpu_labels, labels_mask, expanded_labels)
+    cle.add_images(expanded_labels, flip, expanded_labels)
+
+    expanded_label_image = np.asarray(expanded_labels)
+    if label_image_is_2d is True:
+        expanded_label_image = np.squeeze(expanded_label_image)
+
+    # trim off the expansion that went outside of the tissue
+    if background_mask is not None:
+        expanded_label_image[background_mask] = 0
+
+    return expanded_label_image
