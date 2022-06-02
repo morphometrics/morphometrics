0ea84f0e88e5b4be8d890d8a1914470e5a143e16
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-06-02 09:04:24 +0200

    expose labeling gui as plugin

--- examples/label_expansion.py
+++ examples/label_expansion.py
@@ -1,18 +1,20 @@
 import napari
 import numpy as np
 
-from morphometrics._gui._qt.labeling_widgets import QtLabelingWidget
-
 label_image = np.zeros((100, 100, 100), dtype=int)
 label_image[30:70, 30:70, 30:70] = 1
 label_image[45:55, 45:55, 45:55] = 0
 
 label_image[49:51, 49:51, 49:51] = 2
 
-viewer = napari.Viewer(ndisplay=3)
-viewer.add_labels(label_image)
+background_mask = np.ones_like(label_image, dtype=bool)
+background_mask[30:70, 30:70, 30:70] = 0
 
-labeling_widget = QtLabelingWidget(viewer)
-viewer.window.add_dock_widget(labeling_widget)
+viewer = napari.Viewer(ndisplay=3)
+viewer.add_image(background_mask)
+viewer.add_labels(label_image)
+viewer.window.add_plugin_dock_widget(
+    plugin_name="morphometrics", widget_name="Curate labels"
+)
 
 napari.run()
--- src/morphometrics/_gui/_qt/labeling_widgets.py
+++ src/morphometrics/_gui/_qt/labeling_widgets.py
@@ -14,12 +14,14 @@ class QtLabelingWidget(QWidget):
         super().__init__()
         self._viewer = viewer
         self._layer = None
+        self._background_mask_layer = None
         self._curating = False
 
         # create the label selection widget
         self._label_selection_widget = magicgui(
             self._toggle_curating,
             labels_layer={"choices": self._get_valid_labels_layers},
+            background_mask_layer={"choices": self._get_valid_image_layers},
             call_button="start curating",
         )
 
@@ -58,10 +60,38 @@ class QtLabelingWidget(QWidget):
             The selected labels layer that will be curated. If no layer is selected,
             set to None.
         """
-        if layer == self._layer:
+        if layer is self._layer:
             # if the layer hasn't changed, don't perform the update
             return None
         self._layer = layer
+        self._on_layer_update()
+
+    @property
+    def background_mask_layer(self) -> Optional[napari.layers.Labels]:
+        """The selected labels layer for curation.
+
+        Returns
+        -------
+        layer : Optional[napari.layers.Labels]
+            The selected labels layer that will be curated. If no layer is selected,
+            returns None.
+        """
+        return self._background_mask_layer
+
+    @background_mask_layer.setter
+    def background_mask_layer(self, layer: Optional[napari.layers.Labels]):
+        """The selected image layer for backgroundmaskiung.
+
+        Parameters
+        ----------
+        layer : Optional[napari.layers.Labels]
+            The selected image layer that be used to mask off background when expanding
+            labels
+        """
+        if layer is self._background_mask_layer:
+            # if the layer hasn't changed, don't perform the update
+            return None
+        self._background_mask_layer = layer
         self._on_layer_update()
 
     @property
@@ -92,6 +122,7 @@ class QtLabelingWidget(QWidget):
         # get the values from the selected labels layer
         label_image = self.layer.data
         label_layer_name = self.layer.name
+        background_mask = self._background_mask_layer.data
 
         @thread_worker(connect={"returned": pbar.hide})
         def _expand_selected_labels() -> LayerDataTuple:
@@ -100,6 +131,7 @@ class QtLabelingWidget(QWidget):
                 label_image=label_image,
                 label_values_to_expand=label_values_to_expand,
                 expansion_amount=expansion_amount,
+                background_mask=background_mask,
             )
 
             layer_kwargs = {"name": label_layer_name}
@@ -114,8 +146,13 @@ class QtLabelingWidget(QWidget):
         """callback function that is called when self.layer is updated"""
         pass
 
-    def _toggle_curating(self, labels_layer: napari.layers.Labels):
+    def _toggle_curating(
+        self,
+        labels_layer: napari.layers.Labels,
+        background_mask_layer: napari.layers.Image,
+    ):
         self.layer = labels_layer
+        self.background_mask_layer = background_mask_layer
         self.curating = True
 
     def _get_valid_labels_layers(self, combo_box) -> List[napari.layers.Labels]:
@@ -123,6 +160,13 @@ class QtLabelingWidget(QWidget):
             layer
             for layer in self._viewer.layers
             if isinstance(layer, napari.layers.Labels)
+        ]
+
+    def _get_valid_image_layers(self, combo_box) -> List[napari.layers.Image]:
+        return [
+            layer
+            for layer in self._viewer.layers
+            if isinstance(layer, napari.layers.Image)
         ]
 
     def _on_start_curating(self):
