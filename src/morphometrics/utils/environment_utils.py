f419d2fc74aa8e0c87cf1697c1aefe422aa472e1
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-06-10 13:41:08 +0200

    fix openCL tests (#28)
    * test workflow from apoc
    
    * remove master
    
    * allow opencl tests
    
    * install from conda
    
    * update workflow
    
    * update dep order
    
    * dep order
    
    * only run expand tests
    
    * dep order
    
    * add tests
    
    * change order
    
    * change order
    
    * skip widgets
    
    * skip widgets
    
    * skip only widget test
    
    * run openCL tests first
    
    * update python versions
    
    * import order
    
    * exclude Mac OS
    
    * update python req
    
    * remove references to master

--- src/morphometrics/utils/environment_utils.py
+++ src/morphometrics/utils/environment_utils.py
@@ -1,4 +1,11 @@
 import os
+import platform
 
 # set to true if running on github actions
 on_ci = os.getenv("CI") == "true"
+
+# set to true if running on windows
+on_windows = platform.system() == "Windows"
+
+# set to true if running on Mac OS
+on_macos = platform.system() == "Darwin"
