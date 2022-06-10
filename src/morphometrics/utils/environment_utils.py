5b6352a85ba1e79679796232dcd08c265038a544
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-06-10 11:30:26 +0200

    import order

--- src/morphometrics/utils/environment_utils.py
+++ src/morphometrics/utils/environment_utils.py
@@ -1,4 +1,8 @@
 import os
+import platform
 
 # set to true if running on github actions
 on_ci = os.getenv("CI") == "true"
+
+# set to true if running on windows
+on_windows = platform.system() == "Windows"
