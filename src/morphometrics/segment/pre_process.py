50803c1d8dee2d3a9cf703881bbeeeb18d44fc6e
Maoxia <moirali.nju@gmail.com>
2022-09-24 16:18:19 +0200

    Update pre_process.py
    set segmentation_mask parameter as optional

--- src/morphometrics/segment/pre_process.py
+++ src/morphometrics/segment/pre_process.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 from skimage.exposure import rescale_intensity
@@ -7,9 +7,9 @@ from skimage.transform import rescale
 
 def pre_process_image(
     raw_image: np.ndarray,
-    segmentation_mask: np.ndarray,
     raw_pixel_size: List[float],
     target_pixel_size: List[float],
+    segmentation_mask: Optional[np.ndarray] = None,
 ):
     """Prepare an image for segmentation.
 
@@ -17,13 +17,14 @@ def pre_process_image(
     ----------
     raw_image : np.ndarray
         The image to be pre-processed.
-    segmentation_mask : np.ndarray
-        A mask set to non-zero in the regions where segmentation should be performed.
         All other pixels are set to 0.
     raw_pixel_size : List[float]
         The pixel size in each dimension of the raw image.
     target_pixel_size : List[float]
         The pixel size in each dimension of the segmentaton model was trained on.
+    segmentation_mask : Optional[np.ndarray] = None
+        A mask set to non-zero in the regions where segmentation should be performed.
+        All other pixels are set to 0.
 
     Returns
     -------
@@ -31,7 +32,8 @@ def pre_process_image(
         The pre-processed image.
     """
     # mask the tissue
-    raw_image[np.invert(segmentation_mask.astype(bool))] = 0
+    if segmentation_mask:
+        raw_image[np.invert(segmentation_mask.astype(bool))] = 0
 
     # calculate the scale factor for the pixel size
     scale_factor = np.asarray(target_pixel_size) / np.asarray(raw_pixel_size)
