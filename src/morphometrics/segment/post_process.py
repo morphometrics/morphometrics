59a917a376bacb35aa486f6b67a003f381689345
Maoxia <moirali.nju@gmail.com>
2022-09-24 16:21:02 +0200

    Update post_process.py
    Set segmentation_mask parameter as optional. Implementation does not resize the image back to its initial size for stardist (no parameter to get the initial size).
    
    Problem: is it necessary to resize the rescaled image back to its initial size for stardist, since we need to filter according to # pixels?

--- src/morphometrics/segment/post_process.py
+++ src/morphometrics/segment/post_process.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 import numpy as np
 from skimage import measure
 from skimage.transform import resize
 
 
 def post_process_image(
-    segmented_image: np.ndarray, segmentation_mask: np.ndarray, threshold: int
+    segmented_image: np.ndarray,
+    threshold: int,
+    segmentation_mask: Optional[np.ndarray] = None,
 ):
     """
     Post-processing the segmented image.
@@ -13,12 +17,12 @@ def post_process_image(
     ---------
     segmented_image: np.ndarray
         the segmented image (same object with the same pixel value)
-    segmentation_mask: np.ndarray
-        A mask set to non-zero in the regions where segmentation should be kept.
-        All other pixels are set to 0.
     threshold: int
         the minimal number of pixels, representing the minimal size of objects that we keep.
         Any objects smaller than that size will be deleted (pixel value set to 0).
+    segmentation_mask: Optional[np.ndarray] = None
+        A mask set to non-zero in the regions where segmentation should be kept.
+        All other pixels are set to 0.
 
     Returns
     -------
@@ -26,10 +30,10 @@ def post_process_image(
         the post-processed image
     """
     # resize the segmented image to match the mask size
-    resized_image = resize(segmented_image, segmentation_mask.shape, order=0)
-
-    # mask the background
-    resized_image[np.invert(segmentation_mask.astype(bool))] = 0
+    if segmentation_mask:
+        resized_image = resize(segmented_image, segmentation_mask.shape, order=0)
+        # mask the background
+        resized_image[np.invert(segmentation_mask.astype(bool))] = 0
 
     # filter out the segmented objects blow the threshold size
     labeled = measure.label(resized_image)
