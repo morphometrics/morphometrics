80435c699a7f019d130c3caba99502791c9b87d1
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-12-24 22:40:32 -0800

    add label selection to model

--- src/morphometrics/_gui/models/labeling_model.py
+++ src/morphometrics/_gui/models/labeling_model.py
@@ -1,7 +1,56 @@
-from typing import Optional, Union
+from itertools import cycle
+from typing import Dict, Optional, Tuple, Union
 
+import glasbey
 import napari
-from napari.utils.events import EmitterGroup, Event
+import numpy as np
+from napari.utils.events import EmitterGroup, Event, EventedSet
+
+
+class ColormapManager:
+    def __init__(self):
+        default_colormap, highlight_colormap = self._initialize_colormaps()
+        self._default_colormap = default_colormap
+        self._highlight_colormap = highlight_colormap
+
+    def _initialize_colormaps(
+        self,
+    ) -> Tuple[Dict[Optional[int], np.ndarray], Dict[Optional[int], np.ndarray]]:
+
+        colors = np.asarray(
+            glasbey.create_palette(
+                palette_size=50,
+                as_hex=False,
+                chroma_bounds=(40, 120),
+                lightness_bounds=(50, 90),
+            )
+        )
+        colors_highlight = np.column_stack((colors, np.ones((len(colors),))))
+        colors_highlight_cycler = cycle(colors_highlight)
+
+        colormap_highlight = {i: next(colors_highlight_cycler) for i in range(1000)}
+        colormap_highlight[0] = np.array([0, 0, 0, 0])
+        colormap_highlight[None] = np.array([0, 0, 0, 1])
+
+        colors_default = colors_highlight.copy()
+        colors_default[:, 3] = 0.5
+        colors_default[:, 0:3] = colors_default[:, 0:3] * 0.5
+
+        colors_default_cylcer = cycle(colors_default)
+        colormap_default = {i: next(colors_default_cylcer) for i in range(1000)}
+        colormap_default[0] = np.array([0, 0, 0, 0])
+        colormap_default[None] = np.array([0, 0, 0, 1])
+
+        return colormap_default, colormap_highlight
+
+    def create_highlighted_colormap(
+        self, label_indices: np.ndarray
+    ) -> Dict[Optional[int], np.ndarray]:
+        label_indices = np.asarray(label_indices)
+        new_colormap = self._default_colormap.copy()
+        for index in label_indices:
+            new_colormap[index] = self._highlight_colormap[index]
+        return new_colormap
 
 
 class LabelingModel:
@@ -12,6 +61,10 @@ class LabelingModel:
         self._labels_layer = None
         self._background_mask_layer = None
         self._curating = False
+
+        # managers for selecting labels
+        self._selected_labels = EventedSet()
+        self._colormap_manager = ColormapManager()
 
     @property
     def labels_layer(self) -> Optional[napari.layers.Labels]:
@@ -82,4 +135,42 @@ class LabelingModel:
         if self._curating == curating:
             return
         self._curating = curating
+        if curating:
+            self._on_curation_start()
+        else:
+            self._on_curation_end()
         self.events.curating()
+
+    def _on_curation_start(self):
+        # add the events
+        self._selected_labels.events.changed.connect(self._on_selection_changed)
+        self.labels_layer.mouse_drag_callbacks.append(self._on_click_selection)
+
+        self.labels_layer.color_mode = "direct"
+        self.labels_layer.color = self._colormap_manager._default_colormap
+
+    def _on_curation_end(self):
+        self._selected_labels.events.changed.disconnect(self._on_selection_changed)
+        self.labels_layer.mouse_drag_callbacks.remove(self._on_click_selection)
+
+    def _on_click_selection(self, layer: napari.layers.Labels, event: Event):
+        """Mouse callback for selecting labels"""
+        label_index = layer.get_value(
+            position=event.position,
+            view_direction=event.view_direction,
+            dims_displayed=event.dims_displayed,
+            world=True,
+        )
+        if "shift" in event.modifiers:
+            self._selected_labels.symmetric_difference_update([label_index])
+        else:
+            self._selected_labels._set.clear()
+            self._selected_labels.update([label_index])
+
+    def _on_selection_changed(self, event: Event):
+        if not self.curating:
+            # don't do anything if not curating
+            return
+        self.labels_layer.color = self._colormap_manager.create_highlighted_colormap(
+            list(self._selected_labels)
+        )
