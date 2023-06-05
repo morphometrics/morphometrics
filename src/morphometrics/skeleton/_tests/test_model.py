a57a891b981c04a9aceba259195c2e83fc422c80
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2023-06-05 15:42:31 +0200

    add tests for node/edge attributes

--- src/morphometrics/skeleton/_tests/test_model.py
+++ src/morphometrics/skeleton/_tests/test_model.py
@@ -23,14 +23,29 @@ def test_skeleton_model_parse(make_valid_skeleton_graph):
     # get the skeleton graph and node coordinates
     skeleton = make_valid_skeleton_graph()
 
-    # update an edge property
+    # update an edge attribute
     nx.set_edge_attributes(skeleton, {(0, 1): {"validated": False}})
+
+    # add an edge attribute that should be deleted by the parser
+    # because it isn't passed as an edge_attribute
+    bad_attribute_key = "bad_attribute"
+    bad_attribute_edge = (0, 1)
+    nx.set_edge_attributes(skeleton, {bad_attribute_edge: {bad_attribute_key: False}})
+
+    # add a node attribute
+    nx.set_node_attributes(skeleton, {0: {"good_node": True}})
+
+    # add a node attribute that should be deleted by the parser
+    # because it isn't passed as a node_attribute
+    bad_attribute_node = 0
+    nx.set_node_attributes(skeleton, {bad_attribute_node: {bad_attribute_key: True}})
 
     # parse the skeleton
     scale = (1, 2, 1)
     parsed_skeleton = Skeleton3D.parse(
         graph=skeleton,
         edge_attributes={"validated": True},
+        node_attributes={"good_node": False},
         edge_coordinates_key="edge_coordinates",
         node_coordinate_key="node_coordinate",
         scale=scale,
@@ -44,6 +59,18 @@ def test_skeleton_model_parse(make_valid_skeleton_graph):
 
     # check the default value was given to the edge attribute with missing values
     assert parsed_skeleton.graph.edges[(1, 2)]["validated"] is True
+
+    # check the edge attribute not passed in edge_attributes is removed
+    assert bad_attribute_key not in parsed_skeleton.graph.edges[bad_attribute_edge]
+
+    # check that the original node attributes were not overwritten
+    assert parsed_skeleton.nodes()[0]["good_node"] is True
+
+    # check that the rest of the nodes got the default value
+    assert parsed_skeleton.nodes()[1]["good_node"] is False
+
+    # check that the node attribute not in node_attributes is removed
+    assert bad_attribute_key not in parsed_skeleton.nodes()[bad_attribute_node]
 
     # check that the scale was applied to the node coordinates
     original_node_coordinates = np.stack(
